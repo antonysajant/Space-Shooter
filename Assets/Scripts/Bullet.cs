8b81bb6bd38bd0c1cc878d99f9fba2de8edbf5b4
Antony Sajan <antonysajant@gmail.com>
2025-02-24 23:48:29 +0530

    Update Bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     void Start()
-    {
-        
+    { 
     }
 
     void Update()
     {
-        
+        transform.Translate(Vector3.up * 20f * Time.deltaTime);   
     }
 }
