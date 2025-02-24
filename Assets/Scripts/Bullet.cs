31bb0bca185171149b07320c29679b8c012124a3
Antony Sajan <antonysajant@gmail.com>
2025-02-24 23:55:09 +0530

    Firing

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,6 +8,9 @@ public class Bullet : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.up * 20f * Time.deltaTime);   
+        transform.Translate(Vector3.up * 20f * Time.deltaTime);
+
+        if (transform.position.y > 8)
+            Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -5,6 +6,8 @@ public class Player : MonoBehaviour
     private float horizontalAxis;
     private float verticalAxis;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private GameObject bullet;
+
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
@@ -15,7 +18,14 @@ public class Player : MonoBehaviour
         horizontalAxis = Input.GetAxis("Horizontal");
         verticalAxis = Input.GetAxis("Vertical");
 
+        movement();
 
+        if (Input.GetKeyDown(KeyCode.Space))
+            fire();
+    }
+
+    void movement()
+    {
         transform.Translate(new Vector3(horizontalAxis, verticalAxis, 0) * speed * Time.deltaTime);
 
         if (transform.position.y <= -4.03f)
@@ -23,9 +33,14 @@ public class Player : MonoBehaviour
         else if (transform.position.y >= 0.5f)
             transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
 
-        if(transform.position.x <= -9.35f)
-            transform.position=new Vector3(9.35f,transform.position.y,transform.position.z);
-        else if(transform.position.x >= 9.35f)
+        if (transform.position.x <= -9.35f)
+            transform.position = new Vector3(9.35f, transform.position.y, transform.position.z);
+        else if (transform.position.x >= 9.35f)
             transform.position = new Vector3(-9.35f, transform.position.y, transform.position.z);
+    }
+
+    void fire()
+    {
+        Instantiate(bullet,new Vector3(transform.position.x,transform.position.y + 0.75f,transform.position.z),Quaternion.identity);
     }
 }
