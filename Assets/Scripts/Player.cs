75258e0d3f14e0c99ba3c104ed22baed26b2b4b6
Antony Sajan <antonysajant@gmail.com>
2025-02-24 21:54:30 +0530

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,18 @@ public class Player : MonoBehaviour
 {
     private float horizontalAxis;
     private float verticalAxis;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float speed = 5f;
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontalAxis = Input.GetAxis("Horizontal");
         verticalAxis = Input.GetAxis("Vertical");
 
-        //transform.Translate(transform.position.x + horizontalAxis, transform.position.y + verticalAxis, transform.position.z);
+
+        transform.Translate(new Vector3(1 * horizontalAxis, 1 * verticalAxis, 0) * speed * Time.deltaTime);
     }
 }
