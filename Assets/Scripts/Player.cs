b390e120228e1ecd7af422f52ad1d1325eab01ea
Antony Sajan <antonysajant@gmail.com>
2025-02-24 23:40:46 +0530

    player yaxis movement fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,5 +17,10 @@ public class Player : MonoBehaviour
 
 
         transform.Translate(new Vector3(1 * horizontalAxis, 1 * verticalAxis, 0) * speed * Time.deltaTime);
+
+        if (transform.position.y <= -4.03f)
+            transform.position = new Vector3(transform.position.x, -4.03f, transform.position.z);
+        else if (transform.position.y >= 0f)
+            transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
     }
 }
