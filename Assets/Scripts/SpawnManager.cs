887f89a5faaf1fca722ad3ab7a907da8a6c6f962
Antony Sajan <antonysajant@gmail.com>
2025-02-26 13:06:29 +0530

    SpawnManager setup

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        { 
+        
+        }
+    
+    }
+}
