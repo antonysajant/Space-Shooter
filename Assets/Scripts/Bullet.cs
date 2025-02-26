dd8dcee18e6256c9750a19e85e666be5f2d785e6
Antony Sajan <antonysajant@gmail.com>
2025-02-26 12:50:57 +0530

    Code cleanup

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    void Start()
-    { 
-    }
-
     void Update()
     {
         transform.Translate(Vector3.up * 20f * Time.deltaTime);
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,17 +3,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float speed = 7.5f;
-    void Start()
-    {
-        
-    }
-
-   
     void Update()
     {
         transform.Translate(Vector3.down * speed * Time.deltaTime);
 
-        if (transform.position.y <= -5.5f)
+        if (transform.position.y <= -5.5f)      //randomly spawns enemy above camera
             transform.position = new Vector3(Random.Range(-9.5f, 9.5f), 7.5f, transform.position.z);
     }
 
