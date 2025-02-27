184539c81ac53c174802df5766fef4353f79665b
Antony Sajan <antonysajant@gmail.com>
2025-02-27 20:37:41 +0530

    Enemy Container setup

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
     [SerializeField] private float spawnRate = 5.0f;
+    [SerializeField] private GameObject enemyContainer;
 
     void Start()
     {
@@ -21,7 +22,8 @@ public class SpawnManager : MonoBehaviour
         while (true)
         {
             Vector3 spawnPos = new Vector3(Random.Range(-9.5f,9.5f),7.5f,0);
-            Instantiate(enemy, spawnPos, Quaternion.identity);
+            GameObject newEnemy = Instantiate(enemy, spawnPos, Quaternion.identity);
+            newEnemy.transform.parent = enemyContainer.transform;
             yield return new WaitForSeconds(spawnRate);
         }
     
