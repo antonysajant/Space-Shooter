f9b9fa30bd8f35e6fea5f4a837de9b739879a251
Antony Sajan <antonysajant@gmail.com>
2025-02-27 20:30:54 +0530

    SpawnManager defined

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private float spawnRate = 5.0f;
 
     void Start()
     {
-        
+        StartCoroutine(SpawnRoutine());
     }
 
     void Update()
@@ -17,8 +19,10 @@ public class SpawnManager : MonoBehaviour
     IEnumerator SpawnRoutine()
     {
         while (true)
-        { 
-        
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-9.5f,9.5f),7.5f,0);
+            Instantiate(enemy, spawnPos, Quaternion.identity);
+            yield return new WaitForSeconds(spawnRate);
         }
     
     }
